fc52b0184bd79aef8b2e100cc6d57cf021561b4e
Javier Peña <apenap@miumg.edu.gt>
2024-10-14 16:43:41 -0600

    Commit inicial

---
+++ ProyectoFinal.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Archivo: ProyectoFinal.java
+
+Autor: <Aldo Javier Amado Peña Pérez>
+Carné: <0900-09-9425>
+
+Fecha: <14102024>
+Curso: Programación II
+
+Descripcion:
+<Base Proyecto Final Version 1.0>
+
+*******************************************************************************/
+
+package proyectofinal;
+import java.util.Scanner;
+
+
+public class ProyectoFinal {
+      
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Programa base Proyecto Final");
+        }
+}
+
+
